3b998acf1d327df7bbdf2b0fd57d9e56f3ca11d7
Jihunkim95 <darak551@naver.com>
2024-01-03 16:17:49 +0900

    add:postgre 테이블 생성 스크립트 추가

---
+++ SwiftUI_hello/SwiftUI_hello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_hello
+//
+//  Created by 김지훈 on 2024/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_hello/SwiftUI_hello/SwiftUI_helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_helloApp.swift
+//  SwiftUI_hello
+//
+//  Created by 김지훈 on 2024/01/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
