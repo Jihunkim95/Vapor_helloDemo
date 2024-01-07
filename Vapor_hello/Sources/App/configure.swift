43db1de8c81c064e4a9c78fa1b977130c11d1c32
Jihunkim95 <darak551@naver.com>
2024-01-07 13:52:59 +0900

    add: RDS postgre접속 정보 수정

--- Vapor_hello/Sources/App/configure.swift
+++ Vapor_hello/Sources/App/configure.swift
@@ -7,12 +7,13 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    //연결test시 상위 터미널에서 swift run실행
     // PostgreSQL 데이터베이스 연결 구성
     app.databases.use(.postgres(
-        hostname: "localhost",
-        username: "kimjihun",
-        password: "3274",
-        database: "testhoon"
+        hostname: "my-db-instance.cfwagaqykjab.ap-northeast-2.rds.amazonaws.com",
+        username: "postgres",
+        password: "qwe12345",
+        database: "initial_db"
     ), as: .psql)
     
     app.migrations.add(CreateUser())
