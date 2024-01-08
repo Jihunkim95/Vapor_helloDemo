349f313d273250d85e4106ce2cf5e6b3e468c24c
Jihunkim95 <darak551@naver.com>
2024-01-08 13:19:06 +0900

    feat: MVVM패턴으로 분리

---
+++ SwiftUI_hello/SwiftUI_hello/User/View/UserCreatView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  SwiftUI_hello
+//
+//  Created by 김지훈 on 2024/01/03.
+//
+import SwiftUI
+
+
+
+// View
+struct UserListView: View {
+    @StateObject private var userVM = UserViewModel()
+    @State private var newUserName:String = ""
+    @State private var newEmail:String = ""
+    
+    var body: some View{
+        NavigationStack{
+            VStack{
+                Section(header: Text("사용자 목록").font(.title)){
+                    Divider()
+                    List(userVM.users){ user in
+                        NavigationLink(value: user){
+                            VStack(alignment: .leading){
+                                Text("사용자명: \(user.username)")
+                                    .font(.subheadline)
+                                Text("이메일: \(user.email)")
+                                    .font(.subheadline)
+                            }
+                        }
+
+                    }.padding()
+                    
+                }.navigationDestination(for: User.self) { user in
+                    UserDetailView(userVM: userVM, user: user)
+                }
+                
+                HStack{
+                    Text("사용자")
+                    TextField("사용자 입력", text: $newUserName)
+                        .padding()
+                        .background(.white)
+                }
+                HStack{
+                    Text("이메일")
+                    TextField("이메일 입력", text: $newEmail)
+                        .padding()
+                        .background(.white)
+                }
+                HStack{
+                    Button("입력"){
+                        userVM.addUser(username: newUserName, email: newEmail)
+                    }
+                    .padding()
+                    .foregroundColor(.blue)
+                }
+
+            }
+            .padding()
+            .background(Color(.systemBackground))
+            
+        }.onAppear{
+            userVM.loadUsers()
+        }
+
+        
+    }
+    
+}
+
+#Preview {
+    UserListView()
+
+}
+
+
---
+++ SwiftUI_hello/SwiftUI_hello/User/View/UserDetailView.swift
@@ -0,0 +1,31 @@
+//
+//  UserDetailView.swift
+//  SwiftUI_hello
+//
+//  Created by 김지훈 on 2024/01/08.
+//
+
+import SwiftUI
+
+struct UserDetailView: View {
+    
+    @ObservedObject var userVM: UserViewModel
+    @State var user: User
+
+    
+    var body: some View {
+        VStack{
+            TextField("Username", text: $user.username)
+            TextField("Email", text: $user.email)
+//            TextField("id", value: $user.id, formatter: NumberFormatter())
+                Button("Save") {
+                    // 저장 로직
+                    userVM.updateUser(user: user)
+                }
+        }
+        .navigationTitle(user.username)
+        .navigationBarTitleDisplayMode(.inline)
+
+    }
+}
+
